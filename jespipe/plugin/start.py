46d26ae84b50b8179d11a6cb849a182025777824
Jason Nucciarone <nucci.programming@gmail.com>
2021-06-23 13:51:57 +0000

    Declare type when calling argparse

--- jespipe/plugin/start.py
+++ jespipe/plugin/start.py
@@ -1,11 +1,12 @@
 import argparse
+import json
 
 
 def start():
     """Pull two parameters from stdin; stage and parameters."""
     parser = argparse.ArgumentParser()
-    parser.add_argument("stage", nargs=1)
-    parser.add_argument("parameters", nargs=1)
+    parser.add_argument("stage", nargs=1, type=str)
+    parser.add_argument("parameters", nargs=1, type=json.loads)
     args = parser.parse_args()
 
     # Return tuple in the following format: (stage, dataset, parameters)
