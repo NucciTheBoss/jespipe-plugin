f9faaee276ea1932a80734f9ed6332ee64c16254
Jason Nucciarone <nucci.programming@gmail.com>
2021-07-23 16:07:00 +0000

    Update factory method in LSTM.py

--- model-plugins/RNN/LSTM.py
+++ model-plugins/RNN/LSTM.py
@@ -337,8 +337,7 @@ if __name__ == "__main__":
         test_labels = parameters["model_labels"]
 
         # Pull the name of the attack (Just pull the first example in the list)
-        attack_name = copy.deepcopy(parameters["adver_features"][0])
-        attack_name = attack_name.split("/"); attack_name = attack_name[-2]
+        attack_name = parameters["attack_name"]
 
         # Loop through each of the adversarial examples
         for adversary in parameters["adver_features"]:
